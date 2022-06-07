acab0c2a2b8f313809a58d63f99936b2b8f14760
Ankit Dwij <ankitdwiz010@gmail.com>
2022-06-07 17:18:31 +0530

    sol

---
+++ Greedy/Activity Selction Problem.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++){
+            v.push_back(end[i],start[i]);
+        }
+        
+        int ans =1;
+        sort(v.begin(),v.end());
+        int limit=v[0].first;
+        
+        for(int i=1;i<n;i++){
+            if(v[i].second>limit){
+                limit=v[i].first;
+                ans++;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
